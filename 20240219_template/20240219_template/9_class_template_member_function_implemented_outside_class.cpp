fe7bdea718e6c2980e4de4470832d097ead66ae2
烟锁池塘柳 <146186406+zhlyzjiabjw547479106@users.noreply.github.com>
2024-02-19 20:57:39 +0800

    9_class_template_member_function_implemented_outside_class.cpp

--- 20240219_template/20240219_template/9_class_template_member_function_implemented_outside_class.cpp
+++ 20240219_template/20240219_template/9_class_template_member_function_implemented_outside_class.cpp
@@ -1,40 +1,41 @@
-//#include "9_class_template_person.h"//Fn F7�󱨴���fatal error LNK1120: 2 ���޷��������ⲿ����
-/*���ݴ����б�������һ���һ��ѱ�������еġ����� "void __cdecl test1(void)" (?test1@@YAXXZ) �������˸÷��š���֪��
-�⡰2 ����ָ����void test1()��������д���
-	Person<string, int>p1("��Сң", 18);
+//#include "9_class_template_person.h"//Fn F7后报错：fatal error LNK1120: 2 个无法解析的外部命令
+/*根据错误列表中上面一点的一大堆报错结果中的“函数 "void __cdecl test1(void)" (?test1@@YAXXZ) 中引用了该符号”可知：
+这“2 个”指的是void test1()里面的两行代码
+	Person<string, int>p1("珞小遥", 18);
 	p1.showPerson();
-	������ע�͵�֮��Ͳ��ᱨ��*/
-#include "9_class_template_person.cpp"//�������1����.h��Ϊ.cpp�������ã���Ϊ������������Դ��ģ�һ�㶼�ǰ���Դ�ļ���
-/*�������2���Ƽ��������ã�����.h��.cpp���е�����д��һ�𣬺�׺����Ϊ.hpp�ļ�
-�������main�������ڵ�Դ�ļ�����ǰ��#include "9_class_template_person.hpp"����
-��.hpp������һ��Ĭ�ϵ��ļ����ͣ��Ǵ�������ģ��ķ��ļ���д�����⵱�е�Լ���׳ɵ��ļ���׺��ϰ���ϣ�����Ҳ���ڱ�������ߵġ�ͷ�ļ����ļ��е��У�
-����Ͳ�ȥ���ˣ���� https://www.bilibili.com/video/BV1et411b73Z/?p=180&spm_id_from=pageDriver&vd_source=057c21513e5e5504cbd6236251ad85fb 
-11��10��ʼ������*/
+	把它们注释掉之后就不会报错*/
+#include "9_class_template_person.cpp"//解决方法1：把.h改为.cpp（不常用，因为很少这样包含源码的，一般都是包含源文件）
+/*解决方法2（推荐，更常用）：将.h与.cpp当中的内容写到一起，后缀名改为.hpp文件
+再在这里（main函数所在的源文件）最前面#include "9_class_template_person.hpp"即可
+（.hpp并不是一个默认的文件类型，是处理“类模板的分文件编写”问题当中的约定俗成的文件后缀。习惯上，把它也放在编译器左边的“头文件”文件夹当中）
+这里就不去试了，详见 https://www.bilibili.com/video/BV1et411b73Z/?p=180&spm_id_from=pageDriver&vd_source=057c21513e5e5504cbd6236251ad85fb 
+11：10开始的内容，
+也可以看20240219_class_template_universal_array.sln这个工程里面的内容*/
 
 
-/*�ᵽ������Ĳ���ԭ��
+/*提到的问题的产生原因：
 * 
-* д��9_class_template_person.h���е���Person�еĳ�Ա����
+* 写在9_class_template_person.h当中的类Person中的成员函数
 	Person(T1 name, T2 age);
 	void showPerson();
-�����ڵ���֮ǰ�ǲ��ᴴ���ġ�
+它们在调用之前是不会创建的。
 
-C/C++�ı�����ִ�д���Ĭ��Ϊ����ǰ���󡱵ġ�
-������������#include "9_class_template_person.h"��ʱ�����ͷ�ļ��в�δû��������Person����������Ա�����������ڵ���֮ǰ�ǲ��ᴴ���ģ���
-��ʱ��������δ������9_class_template_person.cpp����ʵ�ֵ����������������Ա������޷��������ⲿ���
+C/C++的编译器执行代码默认为“从前到后”的。
+当编译器看到#include "9_class_template_person.h"的时候，这个头文件中并未没有生成类Person的这两个成员函数（它们在调用之前是不会创建的），
+此时编译器还未见过在9_class_template_person.cpp里面实现的这两个函数，所以报错“无法解析的外部命令”
 
-�����ʽ1ͨ���ñ�����������9_class_template_person.cpp����ʵ�ֵ���������Person�ĳ�Ա����������ˡ��޷��������ⲿ���������
+解决方式1通过让编译器看到在9_class_template_person.cpp里面实现的这两个类Person的成员函数，解决了“无法解析的外部命令”的问题
 */
 
-/*ģ���STL����ң����Ϣ����C++������P133*/
-/*ѧϰģ�壬��Ϊ����STL������ϵͳ�ṩ��ģ��*/
-/*��ģ��ĳ�Ա����������ʵ��*/
-/*��ģ��ķ��ļ���д��
-����Ƚ϶������£����ÿ����д��������һ��.cppԴ�ļ�����*/
+/*模板和STL：《遥感信息处理C++基础》P133*/
+/*学习模板，是为了在STL中运用系统提供的模板*/
+/*类模板的成员函数在类外实现*/
+/*类模板的分文件编写：
+在类比较多的情况下，会把每个类写到单独的一个.cpp源文件当中*/
 
 void test1()
 {
-	Person<string, int>p1("��Сң", 18);
+	Person<string, int>p1("珞小遥", 18);
 	p1.showPerson();
 }
 
